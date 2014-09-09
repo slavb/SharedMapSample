881e05925fdc5c3fd51e216335f4f0eeeab05f96
slavb <sbarouline@hotmail.com>
2014-09-09 15:41:41 +1000

    Deleted not used project

--- SharedMapSample/SharedMapSample.Shared/MainPage.xaml.cs
+++ SharedMapSample/SharedMapSample.Shared/MainPage.xaml.cs
@@ -6,7 +6,7 @@ using Windows.UI.Xaml.Controls;
 using System.Diagnostics;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml;
-using Bing.Maps;
+//using Bing.Maps;
 
 using NuclearPlants;
 
